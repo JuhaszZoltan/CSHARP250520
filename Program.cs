125034b6e2927ed85b332f97d2e403d434adcc0f
Pusztai Sándor <pusztai.sandor@verebelyszki.hu>
2025-05-20 19:22:35 +0200

    Add new name

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 15);
 
-string[] names = ["Zoltán", "Sándor"];
+string[] names = ["Zoltán", "Sándor", "Anikó"];
 
 Console.WriteLine($"This prg created by: {string.Join(", ", names)}");
