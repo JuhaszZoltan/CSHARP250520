6c9ac9c9f51172680ff48b1726904ac0b806a731
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-05-20 19:35:02 +0200

    staaars :33333

--- Program.cs
+++ Program.cs
@@ -3,3 +3,14 @@
 string[] names = ["Zoltán", "Sándor", "Dezső", "Anikó"];
 
 Console.WriteLine($"This prg created by: {string.Join(", ", names)}");
+
+Console.WriteLine("some randompy placed star :333");
+
+for (int i = 0; i < 100; i++)
+{
+    Console.SetCursorPosition(
+        left: Random.Shared.Next(Console.WindowWidth -1),
+        top: Random.Shared.Next(3, Console.WindowHeight));
+    Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 15);
+    Console.Write("*");
+}
